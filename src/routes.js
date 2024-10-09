fe1380963b93b2b71afb899a914eecd4b62873ec
Bimochan Shrestha <bmochan@gmail.com>
2024-10-09 16:41:27 +0545

    feat/rentals: Add rent routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import swaggerSpec from './utils/swagger';
 import userRoutes from './routes/userRoutes';
 import movieRoutes from './routes/movieRoutes';
+import rentalRoutes from './routes/rentalRoutes';
 
 /**
  * Contains all API routes for the application.
@@ -28,5 +29,6 @@ router.get('/', (req, res) => {
 
 router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
+router.use('/rents', rentalRoutes);
 
 export default router;
